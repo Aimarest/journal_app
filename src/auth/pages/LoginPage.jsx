292458595d950776b0a8d29b700a169a54b88c28
Ana <amarcosestevez@gmail.com>
2022-09-12 11:21:28 +0200

    add a form at LoginPAge component

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,41 @@
-
+import { TextField, Typography } from '@mui/material';
+import {Grid} from '@mui/material';
 
 export const LoginPage = () => {
   return (
-    <>
-        <h1>LoginPage</h1>
-  
-    </>
+  <Grid 
+  container
+  spacing={0}
+  direction="column"
+  alignItems="center"
+  justifyContent="center"
+  sx={{ minHeight:'100vh', backgroundColor: 'primary.main', padding:4}}>
+<Grid item
+className='box-shadow'
+xs={3}
+sx={{backgroundColor:'white', padding:3, borderRadius: 2}}>
+  <Typography variant='h5' sx={{ mb:1 }}>Login</Typography>
+  <form>
+    <Grid container>
+      <Grid item xs={12} md={6} sx={{mt:2}}>
+        <TextField 
+        label="Correo" 
+        type="email" 
+        placeholder='correo@google.com'
+        fullwidth 
+        />
+           <Grid item xs={12} md={6} sx={{mt:2}}>
+        <TextField 
+        label="Password" 
+        type="password" 
+        placeholder='Contraseña'
+        fullwidth 
+        />
+      </Grid>
+      </Grid>
+    </Grid>
+  </form>
+</Grid>
+  </Grid>
    ) 
 }
